9b80553a6bb68849bd248fcddc8c153c89158780
Zaheershaik-SDE-1 <shaikzaheer2002@gmail.com>
2026-06-26 11:53:55 +0530

    setState

--- src/Counter.js
+++ src/Counter.js
@@ -43,3 +43,17 @@ class Counter extends Component{
 }
 
 export default Counter;
+
+
+
+
+
+/*Notes : 
+
+Always make use of setState and never modify the state directly.
+
+Code has to be executed after the state has been updated ? place that code in the callback function which is the second argument of the setState method.
+
+When you have to update state based on the previous state value, pass in a function as an argument insted of regular object.
+
+*/
